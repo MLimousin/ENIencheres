b5bb19122b1ccceba8adf52cad4be220f5fed2c6
MLimousin <85169374+MLimousin@users.noreply.github.com>
2021-06-01 15:39:14 +0200

    Add files via upload

--- create_bd_trocencheres.sql
+++ create_bd_trocencheres.sql
@@ -1,7 +1,7 @@
 -- Script de création de la base de données ENCHERES
 --   type :      SQL Server 2012
 --
-
+CREATE DATABASE TrocEncheres;
 
 
 CREATE TABLE CATEGORIES (
@@ -91,3 +91,14 @@ ALTER TABLE ARTICLES_VENDUS
 ON DELETE NO ACTION 
     ON UPDATE no action 
 
+-- ajouter un utilisateur test
+INSERT INTO UTILISATEURS VALUES ('userTest', 'TEST', 'user', 'usertest@mail.fr', '0600000000', 'nom de rue', '44000', 'Nantes', 'Pa$$w0rd', 100,0);
+
+-- ajouter les catégories
+INSERT INTO CATEGORIES VALUES ('Informatique');
+INSERT INTO CATEGORIES VALUES ('Ameublement');
+INSERT INTO CATEGORIES VALUES ('Vêtement');
+INSERT INTO CATEGORIES VALUES ('Sport&Loisirs');
+-- ajout d'articles dans la base pour faire les tests
+INSERT INTO ARTICLES_VENDUS (nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, no_utilisateur,no_categorie) 
+        VALUES ('Fauteuil', 'Fauteuil en cuir', '2021-05-31', '2021-06-15', 100, 1 ,2 );
