13095038a45a4845cb813eaae0146d39fa1fa324
MLimousin <marie.limousin@outlook.com>
2021-06-04 16:52:29 +0200

    page nouvelle vente créée,fonctionne, reste data utilisateurs à mettre

---
+++ src/main/java/fr/eni/ENIencheres/message/LecteurMessage.java
@@ -0,0 +1,44 @@
+package fr.eni.ENIencheres.message;
+
+import java.util.ResourceBundle;
+
+public class LecteurMessage {
+	private static ResourceBundle rb;
+	
+	static
+	{
+		try
+		{
+			rb = ResourceBundle.getBundle("fr.eni.ENIencheres.message.messages_erreur");
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @param code
+	 * @return
+	 */
+	public static  String getMessageErreur(int code)
+	{
+		String message="";
+		try
+		{
+			if(rb!=null)
+			{
+				message = rb.getString(String.valueOf(code));
+			}
+			else
+			{
+				message="Problème à la lecture du fichier contenant les messages";
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			message="Une erreur inconnue est survenue";
+		}
+		return message;
+	}
+}
